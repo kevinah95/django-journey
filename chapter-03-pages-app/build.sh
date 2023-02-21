c2a3b47eb4e2143a25ea28af0a2a3cc01eab6d8d
Kevin Hernández Rostrán <kevinah95@gmail.com>
2023-02-20 18:31:54 -0600

    ci: Add user flag

--- chapter-03-pages-app/build.sh
+++ chapter-03-pages-app/build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 export POETRY_HOME="$(pwd)/.poetry"
 python -m venv $POETRY_HOME
-$POETRY_HOME/bin/pip install poetry==1.2.0
+$POETRY_HOME/bin/pip install poetry==1.2.0 --user
 $POETRY_HOME/bin/poetry --version
 
 $POETRY_HOME/bin/poetry install
