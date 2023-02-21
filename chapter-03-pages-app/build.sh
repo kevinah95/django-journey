f37d324df05b793f3f0f913cc450cf411e46e484
Kevin Hernández Rostrán <kevinah95@gmail.com>
2023-02-20 22:58:37 -0600

    ci: Update reder requirements

--- chapter-03-pages-app/build.sh
+++ chapter-03-pages-app/build.sh
@@ -2,6 +2,16 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+echo "Installing the latest version of poetry..."
+
+pip install --upgrade pip
+
+pip install poetry==1.2.0
+
+rm poetry.lock
+
+poetry lock
+
+python -m poetry install
 
 python manage.py migrate
