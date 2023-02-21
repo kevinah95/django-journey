f28e07535eb838aad94545506624384b167132c2
Kevin Hernández Rostrán <kevinah95@gmail.com>
2023-02-20 19:19:06 -0600

    ci: Change to requirements.txt file

--- chapter-03-pages-app/build.sh
+++ chapter-03-pages-app/build.sh
@@ -2,11 +2,6 @@
 # exit on error
 set -o errexit
 
-export POETRY_HOME="$(pwd)/.poetry"
-python -m venv $POETRY_HOME
-$POETRY_HOME/bin/pip install poetry==1.2.0 --user
-$POETRY_HOME/bin/poetry --version
-
-$POETRY_HOME/bin/poetry install
+pip install -r requirements.txt
 
 python manage.py migrate
